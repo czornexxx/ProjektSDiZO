b21dc02d4021524566badba85d8a9d4f04bb1ba1
rudziik <rudziik@wp.pl>
2014-03-15 17:08:20 +0100

    Update skrypt.bat

--- skrypt.bat
+++ skrypt.bat
@@ -10,8 +10,6 @@ rd/s/q Debug
 cd ..
 cd ..
 
-<<<<<<< HEAD
+
 "H:\Program Files (x86)\Git\bin\sh.exe" --login -i
-=======
-"H:\Program Files (x86)\Git\bin\sh.exe" --login -i
->>>>>>> 146b640ff5d080afd01185494fad8ad9149ff1d2
+
